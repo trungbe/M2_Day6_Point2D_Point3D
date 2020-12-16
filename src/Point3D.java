1dfb96ba57a80360aa3a06ec21808c9eeb15062e
trungbe <trungnd0601@gmail.com>
2020-12-16 14:07:17 +0700

    point

---
+++ src/Point3D.java
@@ -0,0 +1,43 @@
+public class Point3D extends Point2D {
+    private float z = 0.0f;
+
+    public Point3D() {
+    }
+
+    public Point3D(float x, float y, float z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+
+    public void setXYZ(float x, float y, float z) {
+        super.setX(x);
+        super.setY(y);
+        this.z = z;
+    }
+
+    public float[] getXYZ() {
+        float arr[] = new float[3];
+        arr[0] = super.getX();
+        arr[1] = super.getY();
+        arr[2] = this.z;
+        return arr;
+    }
+
+    @Override
+    public String toString() {
+        return "Point3D{" +
+                "x=" + getX() +
+                ", y=" + getY() +
+                ", z=" + z +
+                '}';
+    }
+}
